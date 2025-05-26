9430c81dbbc18fd1d7cfb860ac6f112a9c8d9e8a
Zhorif Fachdiat <zhorif.f.32@gmail.com>
2025-05-27 01:40:35 +0800

    ubah speedometer menjadi speedbar

--- Scripts/General/speedometer.gd
+++ Scripts/General/speedometer.gd
@@ -1,26 +1,28 @@
-extends Node2D
-@export var needle: Sprite2D  
-@export var max_speed: float = 120.0  
-@export var min_angle: float = -150.0  
-@export var max_angle: float = 150.0   
+extends Control
 
-var target_racer: Racer  
+@export var speed_bar: TextureProgressBar
+@export var max_speed: float = 120.0
+
+var target_racer: Node
 
 func _ready():
-	if not needle:
-		needle = get_node("Needle")  
+	if not speed_bar:
+		speed_bar = get_node("SpeedBar")
+	if not speed_bar:
+		printerr("SpeedBar node not found!")
 	if not target_racer:
-		target_racer = get_parent().find_child("Player") 
+		# Cari Player di seluruh scene
+		target_racer = get_tree().get_root().find_child("Player", true, false)
+	if not target_racer:
+		printerr("Player node not found!")
+	if speed_bar:
+		speed_bar.min_value = 0
+		speed_bar.max_value = max_speed
 
 func _process(_delta):
-	if target_racer and needle:
-		
-		var speed = target_racer.ReturnMovementSpeed()
-		
-		var angle = map_value(speed, 0, max_speed, min_angle, max_angle)
-		
-		needle.rotation_degrees = angle
-
-
-func map_value(value: float, from_min: float, from_max: float, to_min: float, to_max: float) -> float:
-	return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
+	if target_racer and speed_bar:
+		if target_racer.has_method("ReturnMovementSpeed"):
+			var speed = target_racer.ReturnMovementSpeed()
+			speed_bar.value = clamp(speed, 0, max_speed)
+		else:
+			printerr("Player node does not have ReturnMovementSpeed()")
